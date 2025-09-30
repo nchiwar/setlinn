6d13be1bb9491696ca05c3e8bf430d73309848dd
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2025-09-30 08:53:58 +0100

    Structure and configuration setup

--- setlinn/src/App.jsx
+++ setlinn/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "@styles/App.css";
+import LoadCircle from "@components/LoadingCircle.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen flex justify-center items-center">
+        <div>
+          <p className="text-2xl text-white bg-slate-400 p-3">
+            Team Visionaries from Afriment building setlinn,{" "}
+            <span className="italic font-bold text-amber-300">
+              the world largest migration community platform.{" "}
+            </span>
+            <span className="mx-3 inline-block align-top">
+              <LoadCircle size={10} iconSize={22} speed={7} items={8} />
+            </span>
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
