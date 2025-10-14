919b1fff14f7b0b60eaf207cf938618f8472d862
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2025-10-14 14:22:02 +0100

    Updated

--- src/pages/HomeLayOut.jsx
+++ src/pages/HomeLayOut.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 // import Navigation from "@components/header/Navigation";
 import NavMenu from "@components/header/NavMenu";
+import Footer from "@components/footer/Footer";
 
 function HomeLayOut() {
   return (
     <div>
       <NavMenu />
       <Outlet />
-      {/* <footer>This is the footer</footer> */}
+      <Footer />
     </div>
   );
 }
