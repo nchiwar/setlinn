00b805fa2871511605938eae483255329309d5d0
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2025-10-21 21:30:24 +0100

    modified

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
+import NavigationBar from "@components/dashboard/NavigationBar.jsx";
+import AppSidebarLayout from "@components/sidebar/AppSidebar";
 
 function DashboardLayout() {
   return (
-    <div className="flex-1 flex flex-col overflow-hidden">
-      <header className="">{/* <p>nav</p> */}</header>
-
-      <main className="p-4 overflow-y-auto flex-1">
-        <Outlet />
-      </main>
-    </div>
+    <AppSidebarLayout>
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <NavigationBar />
+    
+        <main className="flex-1 overflow-y-auto ">
+          <Outlet />
+        </main>
+      </div>
+    </AppSidebarLayout>
   );
 }
 
